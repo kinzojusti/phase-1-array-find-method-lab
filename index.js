b7ef5183cf98eace229badc96ac4199239b1f85c
+ <+>
2022-04-06 13:17:04 -0400

    commit tree 8579e5a6ab2758997a844f612ff922f2238ad950

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+// Write a function called `superbowlWin()` in `index.js`:
+
+// * The function should receive 1 argument, an `Array` of JavaScript `Object`s
+// * Each object has two properties: `year` and `result`
+// * It should use `find()` to test each `Object` to see if the `result` is `"W"` —
+//   a win!
+// * It should return the `year` when the win occurred (if it occurred at all!)
+// * If no win is found, it should return, sadly, `undefined
+
+
+   
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  }
